bdabd1a0dbf9d71a348718b3db77f178548d0d7d
sriram <sriram011@gmail.com>
2022-07-03 05:48:09 +0530

    "First Commit"

---
+++ src/com/welcome/practice/Target.java
@@ -0,0 +1,9 @@
+package com.welcome.practice;
+
+public class Target {
+
+    public static void main(String[] args){
+
+      System.out.println("Welcome to coding world,All the best for your target");
+    }
+}
